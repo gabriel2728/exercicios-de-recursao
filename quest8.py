19822dc6069727bfd25e92c2907015572c4b043c
Gabriel Costa Lima <gabrielcostalimaa@hotmail.com>
2026-05-01 17:51:14 -0300

    fix: adiconando o arquivo quest8.0.py no quest8.py

--- quest8.py
+++ quest8.py
@@ -12,3 +12,31 @@ def contar_caminhos(n, m, i=0, j=0):
     return contar_caminhos(n, m, i + 1, j) + contar_caminhos(n, m, i, j + 1)
 
 print(contar_caminhos(2, 2))
+
+
+# - Desafio extra (com bloqueios)
+print("Desafio extra: com bloqueios")
+def contar_caminhos(matriz, i=0, j=0):
+    n = len(matriz)
+    m = len(matriz[0])
+    
+    # fora da grade
+    if i >= n or j >= m:
+        return 0
+    
+    # célula bloqueada
+    if matriz[i][j] == 1:
+        return 0
+    
+    # chegou no destino
+    if i == n - 1 and j == m - 1:
+        return 1
+    
+    return contar_caminhos(matriz, i + 1, j) + contar_caminhos(matriz, i, j + 1)
+
+grade = [
+    [0, 0],
+    [1, 0]
+]
+
+print(contar_caminhos(grade))
