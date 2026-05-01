660ae49666c7313e4e7524ab6e4a1e7de6112370
Gabriel Costa Lima <gabrielcostalimaa@hotmail.com>
2026-05-01 17:46:49 -0300

    fix: juntando o exercício extra quest3.0.py com quest3.py

--- quest3.py
+++ quest3.py
@@ -19,3 +19,29 @@ print(f"Contagem regressiva a partir de {numero}:")
 
 # inicia a contagem
 contagem_regressiva(numero)
+
+
+# DESAFIO EXTRA
+print("Desafio extra: contagem progressiva recursiva")
+def contagem_progressiva(atual, n):
+    
+    # caso base: quando passa do limite, para a recursão
+    if atual > n:
+        return
+    
+    # imprime o número atual
+    print(atual)
+    
+    # chamada recursiva:
+    # aumenta o valor atual em 1
+    contagem_progressiva(atual + 1, n)
+
+
+# pede um número ao usuário
+numero = int(input("Digite um número positivo inteiro: "))
+
+# mensagem inicial
+print(f"Contagem até {numero}:")
+
+# inicia a contagem a partir do 0
+contagem_progressiva(0, numero)
